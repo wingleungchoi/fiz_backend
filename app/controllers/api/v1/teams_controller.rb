5132676cdf0a90d2cc9044edad2d4bb92777cc7d
wingleungchoi <wingleungchoi@gmail.com>
2017-05-16 16:01:57 +0800

    fix params

--- app/controllers/api/v1/teams_controller.rb
+++ app/controllers/api/v1/teams_controller.rb
@@ -1,9 +1,10 @@
 module Api
   module V1
     class TeamsController < BaseController
+      skip_before_action :verify_authenticity_token
       def winning_chance
         name = team_params['name']
-        win_predictor = WinPredictor.new({team_name: 'Virtus Pro & Ninjas in Pyjamas'})
+        win_predictor = WinPredictor.new({team_name: name})
         chance = win_predictor.calculateChange
         render json: { winning_chance: chance }, status: :ok
       end
